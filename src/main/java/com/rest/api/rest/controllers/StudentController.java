d04c5fb6ad76ecc73694083848c54246a9eb0e17
Anish Srivastava <as0160895@gmail.com>
2024-04-07 01:13:30 +0530

    basic controller code added

--- src/main/java/com/rest/api/rest/controllers/StudentController.java
+++ src/main/java/com/rest/api/rest/controllers/StudentController.java
@@ -1,4 +1,40 @@
 package com.rest.api.rest.controllers;
 
+import com.rest.api.rest.entities.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
 public class StudentController {
+
+    List<Student> studentList=new ArrayList<>();
+
+    @GetMapping("v1/student")
+    public Student getStudent(@RequestParam String rollno){
+        Student student= new Student();
+
+        studentList.forEach(e -> {
+            if(e.getRollno().equalsIgnoreCase(rollno)){
+               student.setName(e.getName());
+               student.setRollno(e.getRollno());
+
+            }
+        });
+        if (student != null) {
+        return student;
+        }
+        return student;
+
+
+    }
+    @PostMapping("v1/student")
+    public List<Student> saveStudent(@RequestBody Student student){
+
+
+        studentList.add(student);
+        return studentList;
+    }
 }
--- src/main/java/com/rest/api/rest/entities/Student.java
+++ src/main/java/com/rest/api/rest/entities/Student.java
@@ -1,4 +1,9 @@
 package com.rest.api.rest.entities;
 
+import lombok.Data;
+
+@Data
 public class Student {
+    private String  rollno;
+    private String name;
 }
