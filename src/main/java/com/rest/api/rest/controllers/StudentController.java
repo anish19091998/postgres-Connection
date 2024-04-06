7aea54bb91d2eb6113be65b0aeefa3b316f3810e
Anish Srivastava <as0160895@gmail.com>
2024-04-07 01:33:11 +0530

    basic services code added

--- src/main/java/com/rest/api/rest/controllers/StudentController.java
+++ src/main/java/com/rest/api/rest/controllers/StudentController.java
@@ -1,40 +1,32 @@
 package com.rest.api.rest.controllers;
 
 import com.rest.api.rest.entities.Student;
+import com.rest.api.rest.services.StudentServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @RestController
 @CrossOrigin("*")
 public class StudentController {
 
-    List<Student> studentList=new ArrayList<>();
+    @Autowired
+    StudentServices studentServices;
+
+
 
     @GetMapping("v1/student")
     public Student getStudent(@RequestParam String rollno){
-        Student student= new Student();
 
-        studentList.forEach(e -> {
-            if(e.getRollno().equalsIgnoreCase(rollno)){
-               student.setName(e.getName());
-               student.setRollno(e.getRollno());
+        return studentServices.getStudent(rollno);
 
-            }
-        });
-        if (student != null) {
-        return student;
-        }
-        return student;
 
 
     }
     @PostMapping("v1/student")
-    public List<Student> saveStudent(@RequestBody Student student){
+    public Student saveStudent(@RequestBody Student student){
+        return studentServices.saveStudent(student);
 
-
-        studentList.add(student);
-        return studentList;
     }
 }
--- src/main/java/com/rest/api/rest/services/impl/StudentServiceImplementation.java
+++ src/main/java/com/rest/api/rest/services/impl/StudentServiceImplementation.java
@@ -1,4 +1,23 @@
 package com.rest.api.rest.services.impl;
 
-public interface StudentServiceImplementation {
+import com.rest.api.rest.entities.Student;
+import com.rest.api.rest.services.StudentServices;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImplementation implements StudentServices {
+    @Override
+    public Student getStudent(String rollno) {
+
+        return null;
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return null;
+    }
+
+
 }
