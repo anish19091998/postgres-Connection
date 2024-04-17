98c2123b6082b502a6dea0147a5c2cd896177833
Anish Srivastava <as0160895@gmail.com>
2024-04-17 23:59:23 +0530

    basic assert for  junit added

--- src/test/java/com/rest/api/rest/RestApplicationTests.java
+++ src/test/java/com/rest/api/rest/RestApplicationTests.java
@@ -1,13 +1,52 @@
 package com.rest.api.rest;
 
+import com.rest.api.rest.entities.Student;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class RestApplicationTests {
 
+
 	@Test
-	void contextLoads() {
+	@DisplayName("comapre asserteqal method")
+	void example1() {
+		int result = someMethod();
+		Assertions.assertEquals(42, result, "The result should be 42");
+	}
+	private int someMethod() {
+		return 42;
+	}
+	@Test
+	void example2(){
+		Integer a= 1;
+		Assertions.assertTrue(1==1,"its true");
+
+	}
+
+	@Test
+	void example3(){
+		Integer a= 1;
+		Assertions.assertFalse(1>2,"its true");
+
+	}
+
+
+	@Test
+	void example4(){
+		Student student=null;
+		Assertions.assertNull(student,"student is null");
+
+	}
+
+	@Test
+	void example5(){
+		Student student=new Student();
+		Assertions.assertNotNull(student,"student is  not null");
+
 	}
 
 }
